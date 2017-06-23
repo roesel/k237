82cd4c2f9d4aef7aa97f2fb9352b1724bf29f5e5
David Roesel <roesel@gmail.com>
2017-06-23 13:53:11 +0200

    pár změn od Zelinky

--- misc.py
+++ misc.py
@@ -76,23 +76,23 @@ def get_range_number(sw_min, sw_max):
     '''Returns source range number for I-source as per page 65 of the Quick reference guide.'''
     range_number = 0
     a = max(abs(sw_min), abs(sw_max))
-    if a < 1e-9:
+    if a <= 1e-9:
         range_number = 1
-    elif a < 1e-8:
+    elif a <= 1e-8:
         range_number = 2
-    elif a < 1e-7:
+    elif a <= 1e-7:
         range_number = 3
-    elif a < 1e-6:
+    elif a <= 1e-6:
         range_number = 4
-    elif a < 1e-5:
+    elif a <= 1e-5:
         range_number = 5
-    elif a < 1e-4:
+    elif a <= 1e-4:
         range_number = 6
-    elif a < 1e-3:
+    elif a <= 1e-3:
         range_number = 7
-    elif a < 1e-2:
+    elif a <= 1e-2:
         range_number = 8
-    elif a < 1e-1:
+    elif a <= 1e-1:
         range_number = 9
     else:
         range_number = 0
